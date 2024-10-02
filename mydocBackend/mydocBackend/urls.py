303819db5d752e34ba13b481d314b4184accdbef
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-10-02 11:40:39 +0300

    registered the models in admin.py

--- mydocBackend/mydocBackend/urls.py
+++ mydocBackend/mydocBackend/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from . import mydoc_api
 
 router = routers.DefaultRouter()
 
@@ -9,6 +8,6 @@ urlpatterns = routers.urls
 
 urlpatterns =+ [
     path('admin/', admin.site.urls),
-    path('api/', include(mydoc_api.urls)),
+    path('api/', include('mydoc_api.urls')),
 
 ]
--- mydocBackend/mydoc_api/admin.py
+++ mydocBackend/mydoc_api/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Doctor, Appointment, AvailableTimeSlot
 
-# Register your models here.
+# Register Doctor model
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'specialization', 'rating', 'fee')
+    search_fields = ('name', 'specialization')
+    list_filter = ('specialization',)
+
+# Register Appointment model
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'patient', 'appointment_date', 'start_time', 'end_time', 'status')
+    search_fields = ('doctor__name', 'patient__username')
+    list_filter = ('status', 'appointment_date')
+    date_hierarchy = 'appointment_date'
+
+# Register AvailableTimeSlot model
+@admin.register(AvailableTimeSlot)
+class AvailableTimeSlotAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'available_date', 'start_time', 'end_time', 'is_available', 'is_booked')
+    search_fields = ('doctor__name',)
+    list_filter = ('available_date', 'is_available', 'is_booked')
+    date_hierarchy = 'available_date'
